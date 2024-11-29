096b9823b35e7455940451b9fd16b076aa0bdf5e
Joep ten Dam <j.tendam@student.tudelft.nl>
2024-11-29 15:59:25 +0100

    Finished to code to update the rainfall data and created the correct data set

--- New model/Rain/Rainloader.py
+++ New model/Rain/Rainloader.py
@@ -2,6 +2,8 @@ import os
 from matplotlib import pyplot as plt
 import pandas as pd
 
+# File used to fix the rainfall data!
+
 def read_file(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
@@ -9,15 +11,15 @@ def read_file(file_path):
         lines = lines[6:]
     
     results = []
-    middle_row = len(lines) // 2
-    middle_col = len(lines[0].split()) // 2
+    num_rows = len(lines)
+    num_cols = len(lines[0].split())
     
     for row_index, line in enumerate(lines):
         values = line.split()
         for col_index, value in enumerate(values):
             if float(value) != 99999.90:
-                lon = 112.0 - (middle_col - col_index) * 0.05
-                lat = -44.5 + (middle_row - row_index) * 0.05
+                lon = 112.0 + col_index * 0.05
+                lat = -44.5 + (num_rows - row_index - 1) * 0.05
                 results.append((lat, lon, float(value)))
     
     results = pd.DataFrame(results, columns=['lat', 'lon', 'Rain mm/y'])
@@ -25,11 +27,19 @@ def read_file(file_path):
     return results
 
 file_path = os.path.join(os.path.dirname(__file__), 'rainan.txt')
-file_path = os.path.join(os.path.dirname(__file__), 'r2011-2020.txt')
 data = read_file(file_path)
 
-fig, ax = plt.subplots(figsize=(8, 5))
-sc = ax.scatter(data['lon'], data['lat'], s=0.1, c=data['Rain mm/y'], cmap='Blues', vmax=1000)
-fig.colorbar(sc, ax=ax, label='Precipition (mm/yr)')
-ax.set(xlabel=r'Longitude ($\degree$E)', ylabel='Latitude ($\degree$N)', aspect='equal')
-plt.show()
+file_path = os.path.join(os.path.dirname(__file__), 'Australia_grid_0p05_data.csv')
+old_data = pd.read_csv(file_path)
+
+# Merge data based on lat and lon
+merged_data = pd.merge(old_data, data, on=['lat', 'lon'], how='left', suffixes=('', '_new'))
+old_data['Rain mm/y'] = merged_data['Rain mm/y_new']
+old_data.interpolate(inplace=True)
+
+old_data.to_csv(file_path, index=False)
+print("Data updated successfully")
+
+
+
+
