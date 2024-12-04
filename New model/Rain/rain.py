595b28af933bf455699ec9c94694fe7229cc5ece
Joep ten Dam <j.tendam@student.tudelft.nl>
2024-12-04 16:17:58 +0100

    Made some progress in the rainfall shit

--- New model/Rain/rain.py
+++ New model/Rain/rain.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+from Rainloader import read_file
 
-file_path = os.path.join(os.path.dirname(__file__), 'Australia_grid_0p05_data.csv')
-data = pd.read_csv(file_path)
+# file_path = os.path.join(os.path.dirname(__file__), "..", "Data", 'dat07_u.csv')
+# data = pd.read_csv(file_path)
+
+
+file_path = os.path.join(os.path.dirname(__file__), "..", "Data", 'rain.txt')
+# file_path = os.path.join(os.path.dirname(__file__), "..", "Data", 'r2011-2020.txt')
+data = read_file(file_path)
 
 fig, ax = plt.subplots(figsize=(8, 5))
 sc = ax.scatter(data['lon'], data['lat'], s=0.1, c=data['Rain mm/y'], cmap='Blues', vmax=1000)
---
+++ New model/Rain/rainChecker.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from Rainloader import read_file
+import numpy as np
+from scipy.spatial import cKDTree
+
+# file_path = os.path.join(os.path.dirname(__file__), "..", "Data", 'dat07_u.csv')
+# data = pd.read_csv(file_path)
+
+
+file_path = os.path.join(os.path.dirname(__file__), "..", "Data", 'rain.txt')
+rain = read_file(file_path)
+
+file_path = os.path.join(os.path.dirname(__file__), "..", 'Data', 'dat07_u.csv')
+df = pd.read_csv(file_path)
+# Create KDTree for fast spatial search
+rain_tree = cKDTree(rain[['lon', 'lat']])
+
+# Query the closest point in rain for each point in df
+distances, indices = rain_tree.query(df[['lon', 'lat']])
+
+# Add the "Rain mm/y" value from the closest point in rain to df
+df['test_rain'] = rain.iloc[indices]['Rain mm/y'].values
+
+# print(data, df)
+
+df['rain_difference'] = df['test_rain'] - df['Rain mm/y']
+    # print(df[['lat', 'lon', 'average_rain', 'Rain mm/y', 'rain_difference']].head())
+print(np.sum(abs((df["rain_difference"])))/len(df["rain_difference"]))
+
+fig, ax = plt.subplots(figsize=(8, 5))
+sc = ax.scatter(df['lon'], df['lat'], s=0.1, c=df['rain_difference'], cmap='viridis', vmin=-100, vmax=100)
+fig.colorbar(sc, ax=ax, label='Precipition (mm/yr)')
+ax.set(xlabel=r'Longitude ($\degree$E)', ylabel='Latitude ($\degree$N)', aspect='equal')
+plt.show()
