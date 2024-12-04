2e4369ee39a0c791e24966ced4316876f9f4335d
char998 <charis.efthmiou@gmail.com>
2024-12-04 17:00:33 +0100

    rf_optimization

--- New model/Improved_RF_code/RF_recharge.py
+++ New model/Improved_RF_code/RF_recharge.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import GridSearchCV
 import os
 from datetime import datetime
 from decimal import Decimal
@@ -79,3 +80,28 @@ def run_rf_model(t_size=0.3, trees=250, max_splits=18, max_features=0.33, min_sa
 
 if __name__ == "__main__":
     run_rf_model(test_data=True)
+
+def optimize_rf_model(X, y, param_grid, cv=5, scoring='neg_mean_squared_error', n_jobs=-1):
+    estimator = RandomForestRegressor()
+    grid_search = GridSearchCV(
+    estimator=estimator,
+    param_grid=param_grid,
+    cv=cv,
+    scoring=scoring,
+    n_jobs = n_jobs,
+    verbose=2
+    )
+    grid_search.fit(X,y)
+    print(f'Best parameters found: {grid_search.best_params_}')
+    print(f'Best score: {grid_search.best_score_}')
+    return grid_search.best_estimator_
+
+hyperparameter_grid = {'n_estimators':[50,100,150,200,250,300,350,400]}
+train_params = ['Rain mm/y', 'rainfall_seasonality', 'PET mm/y', 'elevation_mahd', 'distance_to_coast_km', 'ndvi_avg', 'clay_perc', 'soil_class']
+
+df = pd.read_csv('dat07_u.csv', low_memory=False).sample(frac=1, random_state=42)
+df.dropna(subset=['Rain mm/y', 'koppen_geiger', 'PET mm/y', 'distance_to_coast_km', 'Aridity', 'elevation_mahd', 'wtd_mbgs', 'regolith_depth_mbgs', 'slope_perc', 'clay_perc', 'silt_perc', 'sand_perc', 'soil_class', 'geology', 'ndvi_avg', 'vegex_cat', 'rainfall_seasonality'], inplace=True)
+X = df[train_params]
+y = df['Recharge RC 50% mm/y']
+best_model = optimize_rf_model(X, y, hyperparameter_grid)
+print(f'Optimized model: {best_model}')
