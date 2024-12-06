5454dbb8ceadc3ce2961f8ff2bb5b71b6b6a0a4a
Joep ten Dam <j.tendam@student.tudelft.nl>
2024-12-06 16:26:30 +0100

    Optimized the parameters

--- New model/Improved_RF_code/RF_recharge.py
+++ New model/Improved_RF_code/RF_recharge.py
@@ -6,7 +6,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.experimental import enable_halving_search_cv  # noqa
 from sklearn.model_selection import HalvingGridSearchCV
 from scipy.stats import randint
-from skopt import BayesSearchCV
+# from skopt import BayesSearchCV
 
 import os
 from datetime import datetime
@@ -14,7 +14,7 @@ from decimal import Decimal
 import random
 import joblib
 
-def run_rf_model(t_size=0.3, trees=250, max_splits=18, max_features=0.33, min_samples_leaf=8, min_samples_split=2, k_num=10, y_var='Recharge RC 50% mm/y', y_predict='R50', aus_file='Australia_grid_0p05_data.csv', seed=42, test_data=False):
+def run_rf_model(t_size=0.3, trees=50, max_splits=40, max_features='log2', min_samples_leaf=8, min_samples_split=8, k_num=10, y_var='Recharge RC 50% mm/y', y_predict='R50', aus_file='Australia_grid_0p05_data.csv', seed=42, test_data=False):
     start_time = datetime.now()
     DataLocation = os.path.join(os.path.dirname(__file__), '..', 'data')
     os.chdir(DataLocation)
@@ -30,7 +30,6 @@ def run_rf_model(t_size=0.3, trees=250, max_splits=18, max_features=0.33, min_sa
     random.seed(seed)
     random_num = random.randint(0, 1000)
 
-
     if not test_data:
         X = df[train_params]
         y = df[y_var]
@@ -43,11 +42,10 @@ def run_rf_model(t_size=0.3, trees=250, max_splits=18, max_features=0.33, min_sa
         Xtrain = train_data[['Rain mm/y', 'rainfall_seasonality', 'PET mm/y', 'elevation_mahd', 'distance_to_coast_km', 'ndvi_avg', 'clay_perc', 'soil_class']]
         ytrain = train_data[y_var]
 
-    rf = RandomForestRegressor(n_estimators=trees, max_depth=max_splits, random_state=random_num, max_features=max_features, min_samples_leaf=min_samples_leaf, min_samples_split=min_samples_split, oob_score=True)
+    rf = RandomForestRegressor(n_estimators=trees, max_depth=max_splits, random_state=random_num, max_features=max_features, min_samples_leaf=min_samples_leaf, min_samples_split=min_samples_split, bootstrap=False, oob_score=False)
     rf.fit(Xtrain, ytrain)
 
     print(f'Training Score: {rf.score(Xtrain, ytrain):.3f}')
-    print(f'Out of bag score (R2 Score): {rf.oob_score_:.3f}')
 
     if not test_data:
         ypredv = rf.predict(Xvalid)
@@ -83,7 +81,7 @@ def run_rf_model(t_size=0.3, trees=250, max_splits=18, max_features=0.33, min_sa
         print(f'Model saved to {model_filename}')
 
 if __name__ == "__main__":
-    run_rf_model(test_data=True)
+    run_rf_model(test_data=False)
 
 def optimize_rf_model(X, y, param_grid, cv=5, scoring='neg_mean_squared_error', n_jobs=-1):
     estimator = RandomForestRegressor()
@@ -115,17 +113,17 @@ def Bayes_opt_rf(X,y,param_grid):
     print("Best Score:", bayes_opt.best_score_)
     return bayes_opt.best_estimator_
 
-hyperparameter_grid = {'n_estimators':[100,200,300,400,500,600,700,800,900,1000],
-                        'max_depth': [3,8,13,18,23,28,33,38,43,48,50],
-                        'min_samples_split': [2,5,10,15,20],
-                        'min_samples_leaf': [1,5,10,15,20,25,30,35,40,45,50],
-                        'max_features': [0.1,0.3,0.5,0.7,0.9]   }
+# hyperparameter_grid = {'n_estimators':[100,200,300,400,500,600,700,800,900,1000],
+#                         'max_depth': [3,8,13,18,23,28,33,38,43,48,50],
+#                         'min_samples_split': [2,5,10,15,20],
+#                         'min_samples_leaf': [1,5,10,15,20,25,30,35,40,45,50],
+#                         'max_features': [0.1,0.3,0.5,0.7,0.9]   }
 
-train_params = ['Rain mm/y', 'rainfall_seasonality', 'PET mm/y', 'elevation_mahd', 'distance_to_coast_km', 'ndvi_avg', 'clay_perc', 'soil_class']
+# train_params = ['Rain mm/y', 'rainfall_seasonality', 'PET mm/y', 'elevation_mahd', 'distance_to_coast_km', 'ndvi_avg', 'clay_perc', 'soil_class']
 
-df = pd.read_csv('dat07_u.csv', low_memory=False).sample(frac=1, random_state=42)
-df.dropna(subset=['Rain mm/y', 'koppen_geiger', 'PET mm/y', 'distance_to_coast_km', 'Aridity', 'elevation_mahd', 'wtd_mbgs', 'regolith_depth_mbgs', 'slope_perc', 'clay_perc', 'silt_perc', 'sand_perc', 'soil_class', 'geology', 'ndvi_avg', 'vegex_cat', 'rainfall_seasonality'], inplace=True)
-X = df[train_params]
-y = df['Recharge RC 50% mm/y']
-best_model = Bayes_opt_rf(X, y, hyperparameter_grid)
-print(f'Optimized model: {best_model}')
+# df = pd.read_csv('dat07_u.csv', low_memory=False).sample(frac=1, random_state=42)
+# df.dropna(subset=['Rain mm/y', 'koppen_geiger', 'PET mm/y', 'distance_to_coast_km', 'Aridity', 'elevation_mahd', 'wtd_mbgs', 'regolith_depth_mbgs', 'slope_perc', 'clay_perc', 'silt_perc', 'sand_perc', 'soil_class', 'geology', 'ndvi_avg', 'vegex_cat', 'rainfall_seasonality'], inplace=True)
+# X = df[train_params]
+# y = df['Recharge RC 50% mm/y']
+# best_model = Bayes_opt_rf(X, y, hyperparameter_grid)
+# print(f'Optimized model: {best_model}')
